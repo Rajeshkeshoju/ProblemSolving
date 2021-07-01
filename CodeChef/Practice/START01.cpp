3ff93dfa7b72e737d6e419a4c0203d9a976d70c8
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-07-01 07:44:01 +0530

    Create START01.cpp

---
+++ CodeChef/Practice/START01.cpp
@@ -0,0 +1,16 @@
+//  https://www.codechef.com/problems/START01
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	if(0 <= n && n <= pow(10,5)){
+	    cout<<n;
+	}
+	return 0;
+}
