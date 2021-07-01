c80dab3c52ff2c9fe697f4899446d072f75f8380
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-07-01 07:38:33 +0530

    Create UNKNOWN1.cpp

---
+++ CodeChef/Contests/RLIF2021/UNKNOWN1.cpp
@@ -0,0 +1,21 @@
+//  https://www.codechef.com/RLIF2021/problems/UNKNOWN1
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double a = 0;
+    double b = 0;
+    
+    cin>>a>>b;
+    
+    cout<<fixed;
+    cout.precision(1);
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    
+    return 0;
+}
