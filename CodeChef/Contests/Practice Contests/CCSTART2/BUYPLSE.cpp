b5df806753400c001e8848db4187bed997476e7d
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-07-01 09:00:41 +0530

    Create BUYPLSE.cpp

---
+++ CodeChef/Contests/Practice Contests/CCSTART2/BUYPLSE.cpp
@@ -0,0 +1,13 @@
+//  https://www.codechef.com/CCSTART2/problems/BUYPLSE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a,b,x,y;
+	cin>>a>>b>>x>>y;
+	
+	cout<<(a*x + b*y);
+	return 0;
+}
