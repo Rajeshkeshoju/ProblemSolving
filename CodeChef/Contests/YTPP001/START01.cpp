8325a6e38ba8bbd32e558175a4f9d9079a7e83a6
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-07-04 08:35:36 +0530

    Create START01.cpp

---
+++ CodeChef/Contests/YTPP001/START01.cpp
@@ -0,0 +1,14 @@
+//  https://www.codechef.com/YTPP001/problems/START01
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number = 0;
+	cin >> number;
+	
+	if(1<=number && number<=100000){
+	    cout << number;
+	}
+	return 0;
+}
