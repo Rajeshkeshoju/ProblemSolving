067c11e6aec63fd1d4ba78cdb8f933545ddb2d98
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-07-04 08:36:16 +0530

    Create VALTRI.cpp

---
+++ CodeChef/Contests/YTPP001/VALTRI.cpp
@@ -0,0 +1,23 @@
+//  https://www.codechef.com/YTPP001/problems/VALTRI
+
+#include <iostream>
+using namespace std;
+
+int doesBusGoesToCorrectDirection(int busNumber = 0){
+    return ((busNumber % 5) == 0) || ((busNumber % 6) == 0);
+}
+
+int main() {
+	int busNumber = 0;
+	cin >> busNumber;
+	
+	if(1<=busNumber && busNumber<=1000000){
+	    if(doesBusGoesToCorrectDirection(busNumber)){
+	        cout << "YES";
+	    }else{
+	        cout << "NO";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
