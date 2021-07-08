be7e87de4b5d36a76d33da2191c8cbd5b396b809
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-07-08 17:50:17 +0530

    Create STVQ.cpp

---
+++ CodeChef/Contests/YTPP001/STVQ.cpp
@@ -0,0 +1,45 @@
+//  https://www.codechef.com/YTPP001/problems/STVQ
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct QueryTypes{
+    const int PUSH  = 1;
+    const int POP   = 2;
+    const int PRINT = 3;
+};
+
+int main() {
+	int Q = 0;
+	cin >> Q;
+	
+	if(1<=Q && Q<=2000){
+	    int queryNumber = 0;
+	    QueryTypes queryTypes;
+	    stack<int> stackOfNumers;
+	    int pushItem = 0;
+	    
+	    while(Q--){
+	        cin >> queryNumber;
+    	    
+    	    if(queryNumber == queryTypes.PUSH){
+    	        cin >> pushItem;
+    	        stackOfNumers.push(pushItem);
+    	    }else
+    	    
+    	    if(queryNumber == queryTypes.POP){
+    	        if(!stackOfNumers.empty()){
+    	            stackOfNumers.pop();
+    	        }
+    	    }else
+    	    
+    	    if(queryNumber == queryTypes.PRINT){
+    	        cout << stackOfNumers.top();
+    	        cout << endl;
+    	    }
+    	    
+	    }
+	}
+	return 0;
+}
