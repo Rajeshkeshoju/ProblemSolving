65d10f918b79504a5332eded7ced7b53ac354b0c
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-11-06 22:07:26 +0530

    Create Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.cpp

---
+++ SlidingWindow/Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double avg(int sum, int n){
+        return sum / (double) n;
+    }
+    
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int windowSum = 0;
+        int numOfSubarrays = 0;
+        
+        for(int i=0; i<k; i++){
+            windowSum += arr[i];
+        }
+        
+        if(avg(windowSum, k) >= threshold){
+            numOfSubarrays++;
+        }
+        
+        int size = arr.size();
+        
+        for(int i=k; i<size; i++){
+            windowSum += arr[i] - arr[i-k];
+            
+            if(avg(windowSum, k) >= threshold){
+                numOfSubarrays++;
+            }
+        }
+        
+        return numOfSubarrays;
+    }
+};
