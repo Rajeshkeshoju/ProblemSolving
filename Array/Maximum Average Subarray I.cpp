e25679f10b68ff8b6b90b2a51756c97f9217a5e0
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-11-06 22:15:28 +0530

    Create Maximum Average Subarray I.cpp

---
+++ Array/Maximum Average Subarray I.cpp
@@ -0,0 +1,25 @@
+//  https://leetcode.com/submissions/detail/582493076/
+
+class Solution {
+public:
+    double avg(int sum, int count){
+        return sum / (double) count;
+    }
+    
+    double findMaxAverage(vector<int>& nums, int k) {
+        int windowSum = 0;
+        double maxAverage = 0.0;
+        
+        for(int i=0; i<k; i++){
+            windowSum += nums[i];
+        }
+        
+        int maxSum = windowSum;
+        for(int i=k; i<nums.size(); i++){
+            windowSum += nums[i] - nums[i-k];
+            maxSum = max(maxSum, windowSum);
+        }
+        
+        return avg(maxSum, k);
+    }
+};
