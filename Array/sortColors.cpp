159c7f1b243ea7d7b3317677faac51321f6099f1
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2021-11-15 09:16:00 +0530

    Create sortColors.cpp

---
+++ Array/sortColors.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+void swap(int& a, int& b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+
+void sortArray(int* arr, int n){
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+    
+    while(mid <= high){
+        switch(arr[mid]){
+            case 0:
+                swap(arr[low++], arr[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(arr[mid], arr[high--]);
+                break;
+        }
+    }
+}
+
+int main(){
+    
+    int n = 0;
+    cin >> n;
+    
+    int* arr = new int[n];
+    
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    sortArray(arr, n);
+    
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
