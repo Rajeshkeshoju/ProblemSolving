f5f8970690344bb2a8cba162d2954627f9737a6d
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2022-08-20 23:05:49 +0530

    Create Bubble sort.cpp

---
+++ Sorting/Bubble sort.cpp
@@ -0,0 +1,11 @@
+void bubbleSort(int* arr, int n) {
+    
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            
+            if(arr[i] > arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
